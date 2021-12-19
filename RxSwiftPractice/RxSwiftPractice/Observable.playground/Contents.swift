48d9d6927f1298601b09c880c5a3dba5d5f4f477
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-12-19 22:19:38 +0900

    .
    .

--- RxSwiftPractice/RxSwiftPractice/Observable.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice/Observable.playground/Contents.swift
@@ -52,3 +52,88 @@ Observable<Void>.empty()
         print($0)
     }
 
+print("-----never-----")
+Observable<Void>.never()
+    .debug("never")
+    .subscribe(onNext: {
+            print($0)
+        },
+        onCompleted: {
+            print("Completed")
+        }
+    )
+
+print("-----range-----")
+Observable.range(start: 1, count: 9)
+    .subscribe(onNext: {
+        print("2 * \($0) = \(2*$0)")
+    })
+
+print("-----dispose-----")
+// ()에 값들이 무한하게 많다면 꼭 dispose를 해야 Completed가 출력된다.
+Observable.of(1, 2, 3)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .dispose()
+
+print("-----disposeBag-----")
+//효율적으로 관리 하기 위해
+let disposeBag = DisposeBag()
+Observable.of(1, 2, 3)
+    .subscribe{
+        print($0)
+    }
+    .disposed(by: disposeBag)
+
+print("-----create1-----")
+Observable.create { observable -> Disposable in
+    observable.onNext(1)
+//    observable.on(.next(1))
+    observable.onCompleted()
+//    observable.on(.completed)
+    observable.onNext(2)
+    return Disposables.create()
+}
+.subscribe{
+    print($0)
+}
+.disposed(by: disposeBag)
+
+print("-----create2-----")
+enum MyError: Error {
+    case anError
+}
+
+Observable<Int>.create { observable -> Disposable in
+    observable.onNext(1)
+    observable.onError(MyError.anError)
+    observable.onCompleted()
+    observable.onNext(2)
+    return Disposables.create()
+}
+.subscribe(
+    onNext: {
+        print($0)
+    },
+    onError: {
+        print($0.localizedDescription)
+    },
+    onCompleted: {
+        print("Completed")
+    },
+    onDisposed: {
+        print("Disposed")
+    }
+)
+.disposed(by: disposeBag)
+
+
+print("-----deferred-----")
+Observable.deferred {
+    Observable.of(1, 2, 3)
+}
+.subscribe{
+    print($0)
+}
+.disposed(by: disposeBag)
