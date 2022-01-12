3b205a7a5e348e289d2f36993655d2abdb0a6e2c
김상은 <81676542+SangAu124@users.noreply.github.com>
2022-01-12 14:48:52 +0900

    TimeBasedOperator를 공부하였습니다.
    TimeBasedOperator를 공부하였습니다.

--- RxSwiftPractice/RxSwiftPractice/TimeBasedOperator.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice/TimeBasedOperator.playground/Contents.swift
@@ -1,4 +1,7 @@
 import RxSwift
+import RxCocoa
+import UIKit
+import PlaygroundSupport
 
 let disposeBag = DisposeBag()
 
@@ -34,56 +37,56 @@ let 타임스톤 = 닥터스트레인지.replayAll()
     .disposed(by: disposeBag)
 
 print("---------buffer---------")
-//let source = PublishSubject<String>()
-//
-//var count = 0
-//let timer = DispatchSource.makeTimerSource()
-//
-//timer.schedule(deadline: .now() + 2, repeating: .seconds(1))
-//timer.setEventHandler {
-//    count += 1
-//    source.onNext("\(count)")
-//}
-//timer.resume()
-//source
-//    .buffer(
-//        timeSpan: .seconds(2),
-//        count: 2,
-//        scheduler: MainScheduler.instance
-//    )
-//    .subscribe(onNext: {
-//        print($0)
-//    })
-//    .disposed(by: disposeBag)
+let source = PublishSubject<String>()
+
+var count = 0
+let timer = DispatchSource.makeTimerSource()
+
+timer.schedule(deadline: .now() + 2, repeating: .seconds(1))
+timer.setEventHandler {
+    count += 1
+    source.onNext("\(count)")
+}
+timer.resume()
+source
+    .buffer(
+        timeSpan: .seconds(2),
+        count: 2,
+        scheduler: MainScheduler.instance
+    )
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
 
 print("---------window---------") // buffer와 같지만 return 값이 Observable이다.
-//let 만들어낼최대Observable수 = 5
-//let 만들시간 = RxTimeInterval.seconds(2)
-//
-//let window = PublishSubject<String>()
-//
-//var windowCount = 0
-//let windowTimeSource = DispatchSource.makeTimerSource()
-//windowTimeSource.schedule(deadline: .now() + 2, repeating: .seconds(1))
-//windowTimeSource.setEventHandler {
-//    windowCount += 1
-//    window.onNext("\(windowCount)")
-//}
-//windowTimeSource.resume()
-//
-//window
-//    .window(
-//        timeSpan: 만들시간,
-//        count: 만들어낼최대Observable수,
-//        scheduler: MainScheduler.instance
-//    )
-//    .flatMap { windowObservable -> Observable<(index: Int, element: String)> in
-//        return windowObservable.enumerated()
-//    }
-//    .subscribe(onNext: {
-//        print("\($0.index)번째 Observable의 요소 \($0.element)")
-//    })
-//    .disposed(by: disposeBag)
+let 만들어낼최대Observable수 = 5
+let 만들시간 = RxTimeInterval.seconds(2)
+
+let window = PublishSubject<String>()
+
+var windowCount = 0
+let windowTimeSource = DispatchSource.makeTimerSource()
+windowTimeSource.schedule(deadline: .now() + 2, repeating: .seconds(1))
+windowTimeSource.setEventHandler {
+    windowCount += 1
+    window.onNext("\(windowCount)")
+}
+windowTimeSource.resume()
+
+window
+    .window(
+        timeSpan: 만들시간,
+        count: 만들어낼최대Observable수,
+        scheduler: MainScheduler.instance
+    )
+    .flatMap { windowObservable -> Observable<(index: Int, element: String)> in
+        return windowObservable.enumerated()
+    }
+    .subscribe(onNext: {
+        print("\($0.index)번째 Observable의 요소 \($0.element)")
+    })
+    .disposed(by: disposeBag)
 
 print("---------delaySubscription---------")
 let delaySource = PublishSubject<String>()
@@ -103,3 +106,60 @@ delaySource
         print($0)
     })
     .disposed(by: disposeBag)
+
+print("---------delay---------")
+let delaySubject = PublishSubject<Int>()
+
+var delayCount = 0
+let delayTimerSource = DispatchSource.makeTimerSource()
+delayTimerSource.schedule(deadline: .now(), repeating: .seconds(1))
+delayTimerSource.setEventHandler {
+    delayCount += 1
+    delaySubject.onNext(delayCount)
+}
+delayTimerSource.resume()
+
+delaySubject
+    .delay(.seconds(3), scheduler: MainScheduler.instance)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("---------interval---------")
+Observable<Int>
+    .interval(.seconds(3), scheduler: MainScheduler.instance)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("---------timer---------")
+Observable<Int>
+    .timer(
+        .seconds(5),
+        period: .seconds(2),
+        scheduler: MainScheduler.instance
+    )
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("---------timerout---------")
+let 누르지않으면에러 = UIButton(type: .system)
+누르지않으면에러.setTitle("눌러주세요", for: .normal)
+누르지않으면에러.sizeToFit()
+
+PlaygroundPage.current.liveView = 누르지않으면에러
+
+누르지않으면에러.rx.tap
+    .do(onNext: {
+        print("tap")
+    })
+    .timeout(.seconds(5), scheduler: MainScheduler.instance)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
